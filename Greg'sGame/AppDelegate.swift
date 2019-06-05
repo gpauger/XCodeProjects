25c10c9406e53bc4e1e5aed8136a4dd851e03da5
Greg Auger <gpauger@gmail.com>
2019-06-04 20:03:22 -0700

    Initial Commit

---
+++ Greg'sGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Greg'sGame
+//
+//  Created by Greg Auger on 6/4/19.
+//  Copyright © 2019 gpauger. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
